fc49fcebab196ff2835e5bab04ab7ab8e222ed11
Your Name <you@example.com>
2023-05-04 13:19:40 +0300

    Добавьте файлы проекта.

---
+++ Apteka/DbConnection.cs
@@ -0,0 +1,14 @@
+﻿using Apteka.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Apteka
+{
+    internal class DbConnection
+    {
+        public static AptekaEntities db = new AptekaEntities();
+    }
+}
---
+++ Apteka/Pages/LVPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Apteka
+{
+    /// <summary>
+    /// Логика взаимодействия для LVPage.xaml
+    /// </summary>
+    public partial class LVPage : Page
+    {
+        public LVPage()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+
+            Preparation.ItemsSource = DbConnection.db.Preparation.ToList();
+        }
+
+        private void BtAdd_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new RedactPage());
+        }
+
+        private void BtDataG_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new DGPage());
+        }
+    }
+}
